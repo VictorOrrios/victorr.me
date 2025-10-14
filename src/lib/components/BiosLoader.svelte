193acde2148bbf8bbdd5b3e3e450fbcee732093a
Victor Orrios <victor.orrios.b@gmail.com>
2025-10-14 17:19:23 +0200

    Bios done

--- src/lib/components/BiosLoader.svelte
+++ src/lib/components/BiosLoader.svelte
@@ -10,78 +10,80 @@
     const spinner_states = ["/","—","\\","|"];
 
     let text_library = [
-        "Hola",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Que tal",
-        "Estas"
+        "Hola, que tal estas?",
+        "Doin' sm, idk",
+        "Working on it, don't rush",
+        "Working working working working",
+        "Loading loader to load",
+        "This is a fake loading screen btw",
+        "You are not supposed to read all of this!",
+        "Ain't this a cool animation",
+        "Wait for 32 picoseconds longer",
+        "Only 23.5 hours to go!",
+        "2% complete",
+        "Thinking 100 lines is hard, you got any suggestions?",
+        "This text is 1Kb worth of wasted data",
+        "Screwing screws",
+        "Hammering nails",
+        "Tipping cows",
+        "Spoting UFOS",
+        "Shooting cans",
+        "Drinking milkshake",
+        "Eating ice-cream",
+        "Petting dog",
+        "Petting cat",
+        "Petting cow",
+        "Doing a burnout",
+        "Patching the mainframe firmware",
+        "Hacking the CIA website to display kitties",
+        "Installing cryptominer, kidding, unless",
+        "My fingers hurt writting this fast",
+        "If I did some typo on these don't tell me",
+        "Mopping tears from the floor",
+        "Reading the book my friend gave me 5 years ago",
+        "Watching the series you told me about",
+        "Scratching behind the ears",
+        "Taking a shower",
+        "Horsing around",
+        "Putting all the eggs in one basket",
+        "Dunking from the three point line",
+        "Playing underwatter golf",
+        "Lore impsum blah blah, I forgor the rest",
+        "Pretending this line is funny",
+        "Stealing credit card info",
+        "Getting into a videocall",
+        "Recording audio",
+        "Recording video",
+        "Installing tracker",
+        "Posting about how cool this page is",
+        "Logging IP",
+        "Pulling location",
+        "Snooping around your open tabs",
+        "Following victor on linkedin",
+        "Emailing victor to give him a job position",
+        "Redacting internship acceptance email",
+        "Consulting with the magic 8 ball",
+        "Throwing dice to set random seed",
+        "Throwing something to the wall and seeing what sticks",
+        "Disregarding COORS policy",
+        "DDosing the hackaton API",
+        "Uploading JSON to the db",
+        "Taking a small 5 min break",
+        "Taking turns for the xbox360",
+        "Taking the turn for the ps4",
+        "Taking turns for the nintendo ds",
+        "Hydrating plants",
+        "Scrathing dvds",
+        "Putting hard drives in the microwave",
+        "Smashing ssds",
+        "Polishing marble",
+        "Stealing ASCII art from a website I found",
+        "Watching some youtube",
+        "Coding while in class",
+        "Reading Tailwind docs again",
+        "Consulting with Gepetto",
+        "Asking Emi for help",
+        "Want to add a line? Email/dm me",
     ];
     
     let textBox:line[] = $state([])
@@ -121,8 +123,8 @@
         const r = Math.random();
         let new_state = 0;
         if(r>0.70) new_state = 1;
-        if(r>0.80) new_state = 2;
-        if(r>0.90) new_state = 3;
+        if(r>0.90) new_state = 2;
+        if(r>0.95) new_state = 3;
         if(textBox.length === 0) new_state = 0;
         textBox.push({text:new_text,state:new_state});
         textBox = [...textBox];
@@ -152,16 +154,65 @@
 </script>
 
 <div class="absolute w-[100vw] h-[100vh] bg-black z-10000 bios-container overflow-scroll text-left">
+<!--found on https://textart.sh/topic/crow-->
+<pre class="fixed right-8 bottom-8 text-[1.6vh]! leading-none select-none">                                                                  
+              ░░                                                  
+          ░░  ░░                                                  
+░░░░      ░░░░░░░░                                                
+    ░░░░  ░░▒▒▒▒▒▒░░                                              
+      ▒▒▒▒  ▓▓▒▒▓▓▒▒░░                                            
+      ░░▓▓▒▒▓▓▓▓▓▓▒▒░░▒▒░░░░░░    ░░▒▒██▓▓▓▓██▓▓▒▒░░              
+        ▒▒▓▓▓▓▓▓▒▒▓▓▓▓▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓██████▓▓▒▒░░░░░░░░      
+          ▒▒▓▓▒▒▓▓▓▓▒▒▓▓▓▓▓▓▓▓░░▓▓▓▓▓▓████▓▓▓▓▓▓▓▓▓▓▒▒▒▒░░    ░░  
+          ░░▓▓▓▓████▓▓▓▓▓▓▓▓▒▒▓▓▓▓▒▒▒▒▓▓▓▓████▓▓▒▒▓▓▒▒▒▒░░▒▒▒▒░░  
+            ▒▒▓▓░░▓▓████▓▓▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▓▓██▓▓▒▒▒▒▒▒▒▒░░    
+              ▒▒▒▒▒▒▓▓██▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓████▓▓▓▓▒▒            
+              ▒▒▒▒▒▒▓▓▒▒▒▒▓▓▓▓▒▒████▓▓▓▓▓▓██████▓▓▓▓▓▓▒▒          
+                ▒▒  ▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓██▓▓▓▓▓▓▓▓▓▓▒▒░░      
+                    ▓▓▒▒▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒██████▒▒░░░░        
+                    ▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▒▓▓▒▒▓▓░░    ▓▓▓▓▓▓▒▒          
+                  ▒▒▒▒▒▒▓▓░░▒▒░░▒▒▒▒▓▓▓▓▓▓▓▓▒▒    ▓▓▓▓▒▒▓▓▒▒      
+                  ▒▒▒▒▒▒▓▓▒▒▒▒░░░░▒▒▒▒▒▒▒▒▓▓▒▒      ▓▓▓▓░░░░░░░░  
+                  ▒▒▒▒▓▓▒▒▓▓▓▓▓▓▒▒▒▒▓▓▓▓▓▓▒▒▓▓▒▒      ▒▒▓▓        
+                ░░▓▓▒▒▓▓██▒▒▓▓▒▒▒▒▒▒▒▒▒▒▓▓▒▒▒▒          ▒▒▒▒      
+                ▒▒▓▓▓▓▓▓██  ▒▒▒▒▓▓▒▒▒▒▓▓▓▓░░░░              ▒▒░░  
+                ▒▒██▓▓▒▒    ░░▓▓▒▒▒▒▒▒▒▒▒▒░░                      
+                ░░██▒▒      ▒▒▒▒▒▒▒▒▒▒▓▓▒▒▒▒░░                    
+                ▒▒░░        ▒▒▒▒▒▒▒▒▒▒▓▓▒▒▒▒░░                    
+                ▒▒          ▓▓▒▒▒▒▒▒▓▓▓▓▒▒▒▒░░                    
+                          ▒▒▓▓▒▒▒▒▒▒░░▓▓▒▒▒▒░░                    
+                          ▓▓▒▒▒▒▒▒▒▒▓▓▒▒▒▒▒▒                      
+                        ░░▓▓▒▒░░▒▒▒▒▓▓▓▓▒▒░░░░                    
+                        ░░▓▓▒▒▒▒▒▒▓▓▓▓▓▓▒▒░░░░                    
+                        ░░▒▒▒▒▓▓▓▓▓▓▓▓▓▓▒▒░░                      
+                        ▒▒▒▒▓▓▒▒▒▒▒▒▓▓▓▓▒▒░░                      
+                        ▒▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒░░                      
+                        ▒▒▓▓▓▓▓▓▓▓▒▒▒▒░░░░░░                      
+                        ▒▒██▒▒▓▓▓▓▓▓░░▒▒░░                        
+                        ▓▓▓▓▒▒▓▓▓▓▓▓▒▒░░░░                        
+                        ▒▒▒▒▒▒▓▓░░▒▒▒▒  ░░                        
+                      ░░▓▓▒▒▒▒▒▒  ▒▒▒▒  ░░                        
+                      ▒▒▒▒░░▒▒▒▒  ░░░░                            
+                      ▒▒░░  ▒▒░░  ░░                              
+                    ░░░░░░░░░░░░  ░░                              
+                    ░░░░  ▒▒░░░░  ▒▒                              
+                    ▒▒░░  ░░      ░░                              
+                  ░░░░  ░░░░                                      
+                  ░░    ░░                                        
+</pre>
     <div class="wrapper w-full h-full text-left p-4">
-        <div class="flex items-center logo-div h-24">
-            <img src="favicon.ico" alt="Logo" class="h-full p-4"/>  
-            <div class="h-full">
-                <div class="mb-4">==VICTORR.ME==</div>
-                <div>/victorr/src/+page.svelte</div>
-                <div>@{navigator.userAgent}</div>
-            </div>
-        </div>
-        <div class="grid grid-cols-[4rem_1fr] gap-x-4 w-full mb-12">
+
+<pre class="leading-none"> __     __   
+ \ \   / /o    
+  \ \ / //     ==VICTORR.ME==
+  /\ V /_,-.   
+ o  \_/-(_/    /victorr/src/+page.svelte
+   //          @{navigator.userAgent}
+  (__)       
+
+</pre>
+
+    <div class="grid grid-cols-[4rem_1fr] gap-x-4 w-full pb-12">
             {#each textBox as line, i (i)}
                     {#if line.state === 1}
                         <span></span>
@@ -176,12 +227,14 @@
                             {/if}
                         </div>
                     {/if}
-                    {#if line.state === 0 || line.state === 1}
-                        <span class="">{line.text}</span>   
+                    {#if line.state === 0}
+                        <span class="">{line.text}.</span>   
+                    {:else if line.state === 1}
+                        <span class="">{line.text}...</span>   
                     {:else if line.state === 2}
-                        <span class="text-yellow-400 font-bold">{line.text}</span>   
+                        <span class="text-yellow-400 font-bold">{line.text}.</span>   
                     {:else}
-                        <span class="text-red-400 font-bold">{line.text.toUpperCase()}</span>   
+                        <span class="text-red-400 font-bold">{line.text.toUpperCase()}.</span>   
                     {/if}
             {/each}
             <div class="line-bracket text-center">
@@ -192,6 +245,11 @@
 </div>
 
 <style>
+    pre{
+        background-color: transparent;
+        box-shadow: none;
+    }
+
     .bg-black {
         background-color: var(--color-background);
     }
